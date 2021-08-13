98ae10298e483f170ba041674df3b53b1c34ec3b
Gabriel Carvalho Maia <maiagabrielc@gmail.com>
2021-08-13 14:31:18 -0300

    subindo arquivos que faltavam

---
+++ fundamentos/bloco-05-javascript-dom-eventos-web-storage/dia-01-dom-e-seletores/script.js
@@ -0,0 +1,19 @@
+document.getElementById('header-container').style.backgroundColor = '#00b069';
+
+document.getElementById('footer-container').style.backgroundColor = '#003533';
+
+document.querySelectorAll('.emergency-tasks div h3')[0].style.backgroundColor = '#a500f3';
+
+document.querySelectorAll('.emergency-tasks div h3')[1].style.backgroundColor = '#a500f3';
+
+document.querySelectorAll('.emergency-tasks div')[0].style.backgroundColor = '#ff9f84';
+
+document.querySelectorAll('.emergency-tasks div')[1].style.backgroundColor = '#ff9f84';
+
+document.querySelectorAll('.no-emergency-tasks div h3')[0].style.backgroundColor = '#232525';
+
+document.querySelectorAll('.no-emergency-tasks div h3')[1].style.backgroundColor = '#232525';
+
+document.querySelectorAll('.no-emergency-tasks div')[0].style.backgroundColor = '#f9db5e';
+
+document.querySelectorAll('.no-emergency-tasks div')[1].style.backgroundColor = '#f9db5e';
