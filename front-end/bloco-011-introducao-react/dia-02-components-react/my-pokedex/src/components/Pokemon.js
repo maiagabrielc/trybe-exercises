545336fca4447335ec67bf47fb8b2cfa8570d65c
Gabriel Carvalho Maia <maiagabrielc@gmail.com>
2021-10-09 00:21:59 -0300

    exercicios do dia 2 finalizados

--- front-end/bloco-011-introducao-react/dia-02-components-react/my-pokedex/src/components/Pokemon.js
+++ front-end/bloco-011-introducao-react/dia-02-components-react/my-pokedex/src/components/Pokemon.js
@@ -1,20 +1,34 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
 class Pokemon extends React.Component {
   render() {
-    const { pokemon } = this.props;
+    const { pokemon: { name, type, averageWeight, image, id } } = this.props;
     return (
       <section className="poke-card">
-        <img src={pokemon.image} alt={pokemon.name}></img>
+        <img src={image} alt={name}></img>
         <div>
-          <p>Name: {pokemon.name}</p>
-          <p>Type: {pokemon.type}</p>
-          <p>Weight: {pokemon.averageWeight.value}{pokemon.averageWeight.measurementUnit}</p>
-          <p>Id: {pokemon.id}</p>
+          <p>Name: {name}</p>
+          <p>Type: {type}</p>
+          <p>Weight: {averageWeight.value}{averageWeight.measurementUnit}</p>
+          <p>Id: {id}</p>
         </div>
       </section>
     )
   }
 }
+
+Pokemon.proptype = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.string,
+    type: PropTypes.string,
+    averageWeight: PropTypes.shape({
+      measurementUnit: PropTypes.string,
+      value: PropTypes.number,
+    }),
+    image: PropTypes.string,
+    id: PropTypes.number,
+  }).isRequired,
+};
 
 export default Pokemon;
