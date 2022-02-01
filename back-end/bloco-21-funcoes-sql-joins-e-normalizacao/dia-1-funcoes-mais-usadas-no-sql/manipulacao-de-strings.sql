0ee627edc8f9d46483aa8589fc4a0537e6548b52
Gabriel Carvalho Maia <maiagabrielc@gmail.com>
2022-01-31 23:12:59 -0300

    dia 1 bloco 21 exemplos de manipulação de strings  em SQL

---
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/manipulacao-de-strings.sql
@@ -0,0 +1,14 @@
+SELECT UCASE('oi, eu sou uma string');
+
+SELECT LCASE('Oi, Eu Sou Uma STRING');
+
+SELECT REPLACE('Oi eu sou uma string', 'string', 'cadeia de cara-
+cteres');
+
+SELECT LEFT('Ois, yo soy una string', 3);
+SELECT RIGHT("Hi, I'm a string", 6);
+
+SELECT CHAR_LENGTH('olá eu sou eu');
+SELECT LENGTH('olá eu sou eu');
+
+SELECT SUBSTRING('Oi, eu sou outra string', 5);
