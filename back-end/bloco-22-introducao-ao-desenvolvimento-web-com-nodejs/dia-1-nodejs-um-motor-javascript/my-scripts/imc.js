db182b946fe3f9cd9342353227eecc59a9a06adb
Gabriel Carvalho Maia <maiagabrielc@gmail.com>
2022-02-08 20:40:46 -0300

    instalação de pacote para leitura de dados via linha de comando e edição do código para aceitar input do usuário

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/my-scripts/imc.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/my-scripts/imc.js
@@ -1,5 +1,7 @@
-const weightUser = 70.5;
-const heightUser = 1.65;
+const input = require('readline-sync');
+
+const weightUser = input.questionFloat('Qual o seu Peso em kilos? ');
+const heightUser = input.questionFloat('Qual a sua Altura em metros? ');
 
 const imc = (weight, height) => {
   const result = (weight / height ** 2).toFixed(2);
