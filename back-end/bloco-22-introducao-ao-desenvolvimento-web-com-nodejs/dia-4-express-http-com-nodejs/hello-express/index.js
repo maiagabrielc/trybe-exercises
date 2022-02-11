269466750d05b0b63795cfe2765f51e420ba2173
Gabriel Carvalho Maia <maiagabrielc@gmail.com>
2022-02-11 16:07:00 -0300

    exemplo de como utilizar alguns métodos HTTP e como encadear métodos

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/hello-express/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/hello-express/index.js
@@ -2,12 +2,58 @@ const express = require('express');
 
 const app = express();
 
-app.get('/hello', handleHelloWorldRequest);
+// Rota com caminho '/', utilizando o método GET
 
-app.listen(3001, () => {
-  console.log('Aplicação ouvindo na port 3001');
+app.get('/', function (req, res) {
+  res.send('hello world');
 });
 
-function handleHelloWorldRequest(req, res) {
-  res.status(200).send('Olá Mundo!');
-}
+// Rota com caminho '/', utilizando o método POST
+
+app.post('/', function (req, res) {
+  res.send('hello world!');
+});
+
+// Rota com caminho '/', utilizando o método PUT
+
+app.put('/', function (req, res) {
+  res.send('hello world');
+});
+
+// Rota com caminho '/', utilizando o método DELETE
+
+app.delete('/', function (req, res) {
+  res.send('hello world');
+});
+
+// Rota com caminho '/', para qualquer método HTTP
+
+app.all('/', function (req, res) {
+  res.send('hello world');
+});
+
+// Requisições também podem ser usadas de forma encadeada, para evitar repetir o caminho
+
+app
+  .route('/')
+  .get(function (req, res) {
+    // Requisições para a rota GET '/' são resolvidas aqui
+    res.send('hello world get');
+  })
+  .post(function (req, res) {
+    // Requisições para a rota POST '/' são resolvidas aqui
+    res.send('hello world post');
+  });
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000');
+});
+// app.get('/hello', handleHelloWorldRequest);
+
+// app.listen(3001, () => {
+//   console.log('Aplicação ouvindo na port 3001');
+// });
+
+// function handleHelloWorldRequest(req, res) {
+//   res.status(200).send('Olá Mundo!');
+// }
