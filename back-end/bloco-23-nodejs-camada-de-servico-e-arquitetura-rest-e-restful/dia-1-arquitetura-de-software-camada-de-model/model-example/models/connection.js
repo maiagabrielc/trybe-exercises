4c5b659e7dde15805f4dd8a7c12769d0fbfe355e
Gabriel Carvalho Maia <maiagabrielc@gmail.com>
2022-02-22 21:20:55 -0300

    criação do banco e a conexão com ele e criação do model que fará uso desse banco

---
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/models/connection.js
@@ -0,0 +1,12 @@
+// models/connection.js
+
+const mysql = require('mysql2/promise');
+
+const connection = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: 'Ab123465@',
+  database: 'model_example',
+});
+
+module.exports = connection;
