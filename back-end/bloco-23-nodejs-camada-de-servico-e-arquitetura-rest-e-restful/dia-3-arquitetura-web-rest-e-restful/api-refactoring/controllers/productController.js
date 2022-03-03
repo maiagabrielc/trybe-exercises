ae500853806ae9689966cf31518254ea380c8488
Gabriel Carvalho Maia <maiagabrielc@gmail.com>
2022-03-03 15:57:55 -0300

    dia 3 bloco 23

---
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-e-restful/api-refactoring/controllers/productController.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const ProductModel = require('../models/productModel');
+
+const router = express.Router();
+
+router.get('/list-products', async (req, res, next) => {
+  const products = await ProductModel.getAll();
+
+  res.send(products);
+  next();
+});
+
+router.get('/get-by-id/:id', async (req, res, next) => {
+  const product = await ProductModel.getById(req.params.id);
+
+  res.send(product);
+  next();
+});
+
+router.post('/add-user', async (req, res) => {
+  const { name, brand } = req.body;
+
+  const newProduct = await ProductModel.add(name, brand);
+
+  res.send(newProduct);
+});
+
+router.post('/delete-user/:id', async (req, res) => {
+  const products = await ProductModel.exclude(req.params.id);
+
+  res.send(products);
+});
+
+router.post('/update-user/:id', async (req, res) => {
+  const { name, brand } = req.body;
+
+  const products = await ProductModel.update(req.params.id, name, brand);
+
+  res.send(products);
+});
+
+module.exports = router;
