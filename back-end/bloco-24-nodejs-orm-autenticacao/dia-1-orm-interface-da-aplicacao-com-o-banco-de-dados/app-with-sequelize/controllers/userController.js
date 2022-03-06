a21a13b2a9d515659227b79cff4636bd2c01f4fd
Gabriel Carvalho Maia <maiagabrielc@gmail.com>
2022-03-06 09:17:50 -0300

    integração com express para consultar o db criado a partir dos métodos do sequelize

---
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/app-with-sequelize/controllers/userController.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { User } = require('../models')
+const router = express.Router();
+
+router.get('/', async (_req, res) => {
+  try {
+    const users = await User.findAll();
+
+    return res.status(200).json(users);
+  } catch (error) {
+    console.error('erro', error.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  }
+})
+
+module.exports = router;
---
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/app-with-sequelize/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const userRouter = require('./controllers/userController');
+
+app.use(userRouter);
+
+app.listen(3000, () => {
+  console.log('Ouvindo');
+});
