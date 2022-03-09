a97e10374b96f5c89beb72cd0c45e17ad0817fcf
Gabriel Carvalho Maia <maiagabrielc@gmail.com>
2022-03-08 21:55:13 -0300

    finalização do conteúdo, com testes de integração e ajustes nos models

--- back-end/bloco-24-nodejs-orm-autenticacao/dia-2-orm-associations/associations/config/config.js
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-2-orm-associations/associations/config/config.js
@@ -8,11 +8,13 @@ module.exports = {
     "dialect": "mysql"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.MYSQL_USERNAME,
+    "password": process.env.MYSQL_PASSWORD,
+    "database": "orm_assoc_test_db",
+    "host": process.env.MYSQL_HOST,
+    "dialect": "mysql",
+    // adicione essa linha a sua configuração para omitir mensagens de log no orm
+    "logging": false
   },
   "production": {
     "username": "root",
--- back-end/bloco-24-nodejs-orm-autenticacao/dia-2-orm-associations/associations/models/Address.js
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-2-orm-associations/associations/models/Address.js
@@ -1,17 +1,11 @@
 module.exports = (sequelize, DataTypes) => {
   const Address = sequelize.define('Address', {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     city: DataTypes.STRING,
     street: DataTypes.STRING,
     number: DataTypes.INTEGER,
-    employeeId: {
-      type: DataTypes.INTEGER,
-      foreignKey: true
-    },
+    employeeId: { type: DataTypes.INTEGER, foreignKey: true },
+    // A declaração da Foreign Key é opcional no model
   },
     {
       timestamps: false,
@@ -20,10 +14,8 @@ module.exports = (sequelize, DataTypes) => {
     });
 
   Address.associate = (models) => {
-    Address.belongsTo(models.Employee, {
-      foreignKey: 'employee_id',
-      as: 'employees'
-    });
+    Address.belongsTo(models.Employee,
+      { foreignKey: 'employeeId', as: 'employees' });
   };
 
   return Address;
