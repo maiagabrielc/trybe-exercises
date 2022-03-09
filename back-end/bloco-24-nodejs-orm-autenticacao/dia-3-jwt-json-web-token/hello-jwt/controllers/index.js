c7ce28a56ca705b713cee776224612d132feb9d9
Gabriel Carvalho Maia <maiagabrielc@gmail.com>
2022-03-09 20:20:20 -0300

    finalização do exercício de fixação

--- back-end/bloco-24-nodejs-orm-autenticacao/dia-3-jwt-json-web-token/hello-jwt/controllers/index.js
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-3-jwt-json-web-token/hello-jwt/controllers/index.js
@@ -1,5 +1,13 @@
 const ping = require('./ping');
+const login = require('./login');
+const topSecret = require('./topSecret');
+const me = require('./me');
+const signup = require('./signup');
 
 module.exports = {
+  me,
   ping,
+  login,
+  topSecret,
+  signup,
 };
