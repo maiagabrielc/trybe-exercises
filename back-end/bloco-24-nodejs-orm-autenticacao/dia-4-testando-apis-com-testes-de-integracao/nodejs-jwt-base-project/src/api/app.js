1c7ed0d17a5b26332f1b49630c39d74a2ea3b24f
Gabriel Carvalho Maia <maiagabrielc@gmail.com>
2022-03-10 20:18:06 -0300

    finalização do conteúdo do dia

--- back-end/bloco-24-nodejs-orm-autenticacao/dia-4-testando-apis-com-testes-de-integracao/nodejs-jwt-base-project/src/api/app.js
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-4-testando-apis-com-testes-de-integracao/nodejs-jwt-base-project/src/api/app.js
@@ -2,11 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
 
-/* Aqui, importamos nossa função que valida se o usuário está ou não autenticado */
-const validateJWT = require('./auth/validateJWT');
-
-const PORT = process.env.PORT || 8080;
-
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -14,11 +9,16 @@ app.use(bodyParser.json());
 
 const apiRoutes = express.Router();
 
-apiRoutes.get('/api/posts', validateJWT, routes.getPosts);
+apiRoutes.get('/api/posts', routes.getPosts);
 apiRoutes.post('/api/users', routes.createUsers);
 apiRoutes.get('/api/users', routes.getUsers);
 apiRoutes.post('/api/login', routes.login);
 
 app.use(apiRoutes);
 
-app.listen(PORT, () => console.log(`Conectado na porta ${PORT}`));
+/*
+    Detalhe para a exportação do `app`, já que
+    precisaremos dele nos testes com `chaiHttp` e
+    para rodar nosso `server.js`
+*/
+module.exports = app;
