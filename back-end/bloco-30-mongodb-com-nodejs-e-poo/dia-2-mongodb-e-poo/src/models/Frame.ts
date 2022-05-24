feea22446f083e2bc4b6027149fd8ba5ef2e9979
Gabriel Carvalho Maia <maiagabrielc@gmail.com>
2022-05-24 18:15:13 -0300

    models

---
+++ back-end/bloco-30-mongodb-com-nodejs-e-poo/dia-2-mongodb-e-poo/src/models/Frame.ts
@@ -0,0 +1,19 @@
+
+import { Schema, model as createModel, Document } from 'mongoose';
+import Frame from '../interfaces/Frames';
+import MongoModel from './MongoModel';
+
+interface FrameDocument extends Frame, Document { }
+
+const frameSchema = new Schema<FrameDocument>({
+  material: String,
+  color: String,
+});
+
+class FrameModel extends MongoModel<Frame> {
+  constructor(model = createModel('Armacoes', frameSchema)) {
+    super(model);
+  }
+}
+
+export default FrameModel;
